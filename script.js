571d218762c84bec5e496fba977069dcc15b1649
Jan Viñas Corella <mirabetjan@gmail.com>
2024-09-22 16:14:16 +0200

    Added input and table

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(() => {
+    $("#username-button").on("click", () => {
+        $("#alert-box").hide()
+        $("#repo-table").hide()
+
+        const query = `https://api.github.com/users/${$("#username").val()}/repos`;
+        $.getJSON(query, (data) => {
+            for(index in data){
+                const row = $("<tr></tr>");
+                ["name", "description", "stargazers_count", "watchers_count"].forEach(p => {
+                    const col = document.createElement("td");
+                    col.textContent = data[index][p]
+                    col.style.cursor = "pointer"
+                    row.append(col);
+                })
+                const url = data[index].html_url
+                row.click(function(){
+                    window.open(url)
+                })
+                $("#table-body").append(row);
+            }
+
+            $("#repo-table").show()
+        })
+    })
+
+    $(document).ajaxError(() => {
+        $("#alert-box").show()
+    })
+})
+
